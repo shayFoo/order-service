6aae5e0a96cd426890d6361b657ae8eec4de9383
shyFoo <oasis1975sun@gmail.com>
2025-09-23 14:04:14 +0900

    Add Gradle configuration for Spring Cloud and CI/CD pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
 plugins {
     java
     id("org.springframework.boot") version "3.5.6"
@@ -17,6 +19,8 @@ java {
 repositories {
     mavenCentral()
 }
+
+extra["springCloudVersion"] = "2025.0.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
@@ -45,4 +49,29 @@ tasks.withType<Test> {
 
 tasks.withType<JavaCompile> {
     inputs.files(tasks.named("processResources"))
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+// cloud native buildpack settings
+tasks.named<BootBuildImage>("bootBuildImage") {
+    environment = mapOf(
+        "BP_JVM_VERSION" to "25",
+        "BP_JVM_TIMEZONE" to "Asia/Tokyo",
+        "LANG" to "ja_JP.UTF-8",
+        "LANGUAGE" to "ja_JP:ja",
+        "LC_ALL" to "ja_JP.UTF-8",
+    )
+    imageName = project.name + ":" + project.version
+    docker {
+        publishRegistry {
+            username = project.findProperty("registryUsername")?.toString()
+            password = project.findProperty("registryToken")?.toString()
+            url = project.findProject("registryUrl")?.toString()
+        }
+    }
 }
