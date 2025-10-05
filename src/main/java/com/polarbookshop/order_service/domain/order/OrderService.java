705a5ce1df64cd2b5bbae601dfbc840153e84207
shyFoo <oasis1975sun@gmail.com>
2025-10-05 16:26:04 +0900

    Enhance order submission with transaction support and event publishing

--- src/main/java/com/polarbookshop/order_service/domain/order/OrderService.java
+++ src/main/java/com/polarbookshop/order_service/domain/order/OrderService.java
@@ -1,8 +1,10 @@
 package com.polarbookshop.order_service.domain.order;
 
 import com.polarbookshop.order_service.api.book.BookClient;
+import com.polarbookshop.order_service.event.bridge.OrderAcceptedEventPublisher;
 import com.polarbookshop.order_service.event.message.OrderDispatchedMessage;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -10,21 +12,25 @@ import reactor.core.publisher.Mono;
 public class OrderService {
     private final OrderRepository repository;
     private final BookClient bookClient;
+    private final OrderAcceptedEventPublisher eventPublisher;
 
-    public OrderService(OrderRepository repository, BookClient bookClient) {
+    public OrderService(OrderRepository repository, BookClient bookClient, OrderAcceptedEventPublisher eventPublisher) {
         this.repository = repository;
         this.bookClient = bookClient;
+        this.eventPublisher = eventPublisher;
     }
 
     public Flux<Order> getAllOrders() {
         return repository.findAll();
     }
 
+    @Transactional
     public Mono<Order> submitOrder(String isbn, int quantity) {
         return bookClient.getBookByIsbn(isbn)
                 .map(book -> book.toAcceptedOrder(quantity))
                 .defaultIfEmpty(Order.rejected(0, isbn, quantity))
-                .flatMap(repository::save);
+                .flatMap(repository::save)
+                .doOnNext(eventPublisher::publishOrderAcceptedEvent);
     }
 
     public Flux<Order> consumeOrderDispatchedEvent(Flux<OrderDispatchedMessage> flux) {
